b12f68a96ac5e09c4827e9b4de78d0900dd6016b
叶刚 <yegang@yegangdemacbook-pro.local>
2016-03-14 09:51:38 +0800

    Initial Commit

---
+++ DumplingProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DumplingProject
+//
+//  Created by 叶刚 on 16/3/14.
+//  Copyright © 2016年 dumplingproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DumplingProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DumplingProject
+//
+//  Created by 叶刚 on 16/3/14.
+//  Copyright © 2016年 dumplingproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
