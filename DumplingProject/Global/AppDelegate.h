204f602880fcf04dfa10999f5609f8c6ffa4477e
guxs1129 <guxs1129@163.com>
2016-03-24 17:34:23 +0800

    test
    test

--- DumplingProject/Global/AppDelegate.h
+++ DumplingProject/Global/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by 叶刚 on 16/3/14.
 //  Copyright © 2016年 dumplingproject. All rights reserved.
 //
-
+#define ss @"this is a test"
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
